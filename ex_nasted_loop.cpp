9f29abf648f17f6c15ee784f85103e60682b68f7
shohan12727 <aishohan001@gmail.com>
2025-02-08 14:47:57 +0600

    update code

---
+++ ex_nasted_loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int n = 20;
+    for (int i = 1; i<=n; i++) //line 
+    {
+        int m = 6
+        ;
+        for (int j = 1; j <=m; j++)
+        {
+           cout << "*"  ;
+        }
+        cout << endl;
+        
+    }
+   
+    
+}
